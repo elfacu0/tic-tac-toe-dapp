33dd091d8ab0a1f23a7ed07d65f2b17861b09976
“elfacu0” <mahat.mejor@gmail.com>
2023-05-12 11:21:01 -0500

    Add chat to game component

--- components/organisms/game/index.tsx
+++ components/organisms/game/index.tsx
@@ -4,7 +4,7 @@ import {
   useAccount,
   useNetwork,
 } from '../../../wallet'
-import { Grid, SetGame, WhoseTurn } from '../../molecules'
+import { Chat, Grid, SetGame, WhoseTurn } from '../../molecules'
 import styles from './style.module.css'
 
 const Game: FunctionComponent = () => {
@@ -17,26 +17,36 @@ const Game: FunctionComponent = () => {
   return (
     <div className={styles.content}>
       {account &&
-        <>
-          <SetGame
-            deployerId={Constants.DeployerId}
-            gameId={gameId}
-            networkPassphrase={networkPassphrase}
-            account={account.address}
-            setGameId={setGameId}
-          />
-          <Grid tokenId={Constants.TokenId}
-            deployerId={Constants.DeployerId}
-            gameId={gameId}
-            networkPassphrase={networkPassphrase}
-            account={account.address} />
-          <WhoseTurn
-            tokenId={Constants.TokenId}
-            gameId={gameId}
-            networkPassphrase={networkPassphrase}
-            account={account.address}
-          />
-        </>
+        <div className={styles.row}>
+          <div className={styles.column}>
+          <Chat
+              deployerId={Constants.DeployerId}
+              gameId={gameId}
+              networkPassphrase={networkPassphrase}
+              account={account.address}
+            />
+          </div>
+          <div className={styles.column}>
+            <SetGame
+              deployerId={Constants.DeployerId}
+              gameId={gameId}
+              networkPassphrase={networkPassphrase}
+              account={account.address}
+              setGameId={setGameId}
+            />
+            <Grid tokenId={Constants.TokenId}
+              deployerId={Constants.DeployerId}
+              gameId={gameId}
+              networkPassphrase={networkPassphrase}
+              account={account.address} />
+            <WhoseTurn
+              tokenId={Constants.TokenId}
+              gameId={gameId}
+              networkPassphrase={networkPassphrase}
+              account={account.address}
+            />
+          </div>
+        </div>
       }
     </div>
   )
